fef2e2b3b4f2a90d356c17121173208ee10eacfe
“elisejonkers” <elisejonkers18@hotmail.com>
2023-12-08 14:50:10 +0100

    fixed countdown to days 99+

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,19 +4,27 @@ import vendor_icon from "../assets/vendor-icon.png";
 import CountDown from "../components/CountDown";
 import { AuthContext } from "../context/auth.context";
 import { useContext, useEffect, useState } from "react";
-import axios from "axios";
+import axios from "axios"
 
 function HomePage(){
     const [userDetails, setUserDetails] = useState({})
     const {user} = useContext(AuthContext)
     const {weddingDate} = user
-
+    
+    const dateToFormat = new Date(weddingDate)
+    const options = {weekday: 'long', month: 'long', day: 'numeric', year: 'numeric'}
+    const formattedWeddingDate = dateToFormat.toLocaleDateString('en-US', options)
+    
     return(
         <>
          
             <h1>I do Planner</h1>
             <div>
+                <div>
+                <h1>{user.name} & {user.namePartner}</h1>
+                <h2>{formattedWeddingDate}</h2>
                 <CountDown weddingDate={weddingDate}/>
+                </div>
                 <Link to={`/GuestList`}>
                     <img src={guest_icon} alt="Guest Icon" />
                     Guest List
