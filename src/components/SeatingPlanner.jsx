74db6a9e21608e31c1bced63fc4503ff4b3a73ed
Júlia Sendra <juliasendra98@gmail.com>
2023-12-10 23:55:37 +0100

    store changes of seating planner tables on backend

--- src/components/SeatingPlanner.jsx
+++ src/components/SeatingPlanner.jsx
@@ -3,9 +3,9 @@ import axios from "axios";
 import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
 
 function SeatingPlanner() {
+  const API_URL = "http://localhost:5005/api";
   const storedToken = localStorage.getItem("authToken");
   let guestsList;
-  let tablesListResponse;
   const [tablesList, setTablesList] = useState([]);
   let unassignedTable;
 
@@ -19,22 +19,15 @@ function SeatingPlanner() {
   useEffect(() => {
     const loadData = async () => {
       try {
-        const guestsResponse = await axios.get(
-          "http://localhost:5005/api/guests",
-          {
-            headers: { Authorization: `Bearer ${storedToken}` },
-          }
-        );
+        const guestsResponse = await axios.get(`${API_URL}/guests`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        });
         guestsList = guestsResponse.data;
 
-        const tablesResponse = await axios.get(
-          "http://localhost:5005/api/seatingTables",
-          {
-            headers: { Authorization: `Bearer ${storedToken}` },
-          }
-        );
-        tablesListResponse = tablesResponse.data;
-        filterUnassignedGuests(guestsResponse.data);
+        const tablesResponse = await axios.get(`${API_URL}/seatingTables`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        });
+        filterUnassignedGuests(guestsResponse.data, tablesResponse.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -43,23 +36,25 @@ function SeatingPlanner() {
     loadData();
   }, []);
 
-  const filterUnassignedGuests = (guestsArray) => {
-    const filteredGuests = guestsArray.filter(guest => guest.seatingTable == null);
-    createUnassignedTable(filteredGuests);
-  }
+  const filterUnassignedGuests = (guestsArray, tablesArray) => {
+    const filteredGuests = guestsArray.filter(
+      (guest) => guest.seatingTable === null
+    );
+    createUnassignedTable(filteredGuests, tablesArray);
+  };
 
-  const createUnassignedTable = (unassignedGuests) => {
+  const createUnassignedTable = (unassignedGuests, tablesArray) => {
     if (unassignedTable != undefined) return;
 
     unassignedTable = {
-        _id: "-1",
-        nameOfTable: "Unassigned Guests",
-        assignedGuests: unassignedGuests
+      _id: "-1",
+      tableName: "Unassigned Guests",
+      assignedGuests: unassignedGuests,
     };
 
-    tablesListResponse.unshift(unassignedTable);
-    setTablesList(tablesListResponse);
-  }
+    tablesArray.unshift(unassignedTable);
+    setTablesList(tablesArray);
+  };
 
   const onDragEnd = (result) => {
     const { source, destination } = result;
@@ -69,7 +64,7 @@ function SeatingPlanner() {
     }
 
     if (source.droppableId === destination.droppableId) {
-      const sourceTable = tables.find(
+      const sourceTable = tablesList.find(
         (table) => table._id === source.droppableId
       );
       const reorderedGuests = reorder(
@@ -78,11 +73,12 @@ function SeatingPlanner() {
         destination.index
       );
       sourceTable.assignedGuests = reorderedGuests;
+      updateTable(sourceTable);
     } else {
-      const sourceTable = tables.find(
+      const sourceTable = tablesList.find(
         (table) => table._id === source.droppableId
       );
-      const destinationTable = tables.find(
+      const destinationTable = tablesList.find(
         (table) => table._id === destination.droppableId
       );
       const draggedGuest = sourceTable.assignedGuests[source.index];
@@ -92,14 +88,57 @@ function SeatingPlanner() {
         draggedGuest
       );
       sourceTable.assignedGuests.splice(source.index, 1);
+
+      let newTableId = destinationTable._id;
+      if (newTableId === "-1") {
+        draggedGuest.seatingTable = null;
+      } else {
+        draggedGuest.seatingTable = destinationTable._id;
+      }
+
+      updateDatabase(sourceTable, destinationTable, draggedGuest);
     }
   };
+
+  const updateDatabase = (table1, table2, guest) => {
+    updateGuest(guest);
+
+    if (table1._id != "-1") {
+      updateTable(table1);
+    }
+
+    if (table2._id != "-1") {
+        updateTable(table2);
+    }
+  };
+
+  const updateGuest = (guest) => {
+    axios
+    .put(`${API_URL}/guests/${guest._id}`, guest, {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    })
+    .then(() => {
+      console.log("Guest edited successfully");
+    })
+    .catch((error) => console.log(error));
+  }
+
+  const updateTable = (table) => {
+    axios
+    .put(`${API_URL}/seatingTables/${table._id}`, table, {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    })
+    .then(() => {
+      console.log("Table edited successfully");
+    })
+    .catch((error) => console.log(error));
+  }
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       {tablesList.map((table) => (
         <div className="seatingPlanner">
-          <h1>{table.nameOfTable}</h1>
+          <h1>{table.tableName}</h1>
           <Droppable droppableId={table._id} direction="horizontal">
             {(droppableProvided) => (
               <ul
@@ -115,6 +154,7 @@ function SeatingPlanner() {
                   >
                     {(draggableProvided) => (
                       <li
+                        key={guest._id}
                         {...draggableProvided.draggableProps}
                         ref={draggableProvided.innerRef}
                         {...draggableProvided.dragHandleProps}
