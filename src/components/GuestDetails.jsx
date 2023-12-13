9f90a0b9ede1bd4995ae33a6a65a6c28636286df
“elisejonkers” <elisejonkers18@hotmail.com>
2023-12-13 18:21:47 +0100

    first styling of guest and vendor details, added icons for categories

--- src/components/GuestDetails.jsx
+++ src/components/GuestDetails.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import toast from "react-hot-toast";
+import delete_icon from "../assets/delete-icon.png";
+import edit_icon from "../assets/edit-icon.png";
 
 function GuestDetails() {
   const [guest, setGuest] = useState({});
@@ -11,7 +13,7 @@ function GuestDetails() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    storedToken
+    storedToken;
     axios
       .get(`${import.meta.env.VITE_API_URL}/api/guests/${guestId}`, {
         headers: { Authorization: `Bearer ${storedToken}` },
@@ -25,58 +27,83 @@ function GuestDetails() {
   }, [guestId]);
 
   const deleteGuest = (guest) => {
-    storedToken
+    storedToken;
 
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this guest?"
     );
     if (confirmDelete) {
       axios
-      .delete(`${import.meta.env.VITE_API_URL}/api/guests/${guest._id}`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      .then((response) => {
-        setGuestToDelete(response.data);
-        console.log("Guest deleted");
-        toast.success("Guest deleted successfully");
-        navigate(`/GuestList`);
-      })
-      .catch((error) => {
-        console.error("Error deleting guest:", error);
-      });
+        .delete(`${import.meta.env.VITE_API_URL}/api/guests/${guest._id}`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then((response) => {
+          setGuestToDelete(response.data);
+          console.log("Guest deleted");
+          toast.success("Guest deleted successfully");
+          navigate(`/GuestList`);
+        })
+        .catch((error) => {
+          console.error("Error deleting guest:", error);
+        });
     }
   };
 
   return (
     <>
-      <div>
-        <h2>Guest Details</h2>
-        <p>
-          Name: {guest.firstName} {guest.lastName}
-        </p>
-        <p>Age: {guest.age}</p>
+      <div className="details-container">
+        <div className="avatar placeholder">
+          <div className="bg-secondary text-neutral-content rounded-full w-24">
+            <span className="text-3xl">
+              FL
+              {/* {guest.firstName[0]}
+              {guest.lastName[0]} */}
+            </span>
+          </div>
+        </div>
+        <div className="details-name">
+          <h1 >
+            {guest.firstName} {guest.lastName}
+          </h1>
+          </div>
+          <div className="details-text">
+          <p>Age: {guest.age}</p>
+          <p>Notes: {guest.notes}</p>
+          <p>Attending: {guest.attending}</p>
+        </div>
+        <div className="details-text">
+          <h3>Contact information:</h3>
         <p>Email: {guest.email}</p>
         <p>Phone number: {guest.phoneNumber}</p>
-        <p>Notes: {guest.notes}</p>
-        <p>Attending: {guest.attending}</p>
-      </div>
-
-      <div className="mt-4">
-        <Link to={`/GuestEdit/${guest._id}`}>
-          <button className="btn btn-xs sm:btn-sm md:btn-md btn-wide">
-            Edit
-          </button>
-        </Link>
-      </div>
-
-      <button
-        className="btn btn-xs sm:btn-sm md:btn-md btn-wide "
-        onClick={() => {
-          deleteGuest(guest);
-        }}
-      >
-        Delete
-      </button>
+        </div>
+       
+        <div className="details-btn">
+          <Link to={`/GuestEdit/${guest._id}`}>
+            <button className="btn">
+            <img
+          className="details-icons"
+          src={edit_icon}
+          al="EditGuest"
+          />
+            </button>
+          </Link>
+        </div>
+        <div>
+        <button
+          className="btn"
+          onClick={() => {
+            deleteGuest(guest);
+          }}
+        >
+          <img
+          className="details-icons"
+          src={delete_icon}
+          al="DeleteGuest"
+          />
+        </button>
+        </div>
+        </div>
+      
     </>
   );
 }
--- src/components/VendorDetails.jsx
+++ src/components/VendorDetails.jsx
@@ -2,21 +2,62 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import toast from "react-hot-toast";
+import delete_icon from "../assets/delete-icon.png";
+import edit_icon from "../assets/edit-icon.png";
+
+import decoration_icon from "../assets/decoration-icon.png"
+import photo_icon from "../assets/photo-icon.png"
+import music_icon from "../assets/vendor-music-icon.png"
+import food_icon from "../assets/food-icon.png"
+import beauty_icon from "../assets/beauty-icon.png"
+import official_icon from "../assets/official-icon.png"
+import location_icon from "../assets/vendor-location-icon.png"
+import dress_icon from "../assets/dress-icon.png"
+import invitation_icon from "../assets/vendor-invitation-icon.png"
+import gift_icon from "../assets/gift-icon.png"
+import default_icon from "../assets/default-icon.jpeg"
 
 function VendorDetails() {
   const [vendor, setVendor] = useState({});
+  const [icon, setIcon] = useState(default_icon)
   const { vendorId } = useParams();
   const storedToken = localStorage.getItem("authToken");
   const navigate = useNavigate();
 
+  const setImage = (category) => {
+    
+    if (category === "Decoration") {
+      return setIcon(decoration_icon)
+    } else if (category === "Photographer") {
+      return setIcon(photo_icon)
+    } else if (category === "Music"){
+      return setIcon(music_icon)
+    } else if (category === "Food"){
+      return setIcon(food_icon)
+    } else if (category === "Beauty & Health"){
+      return setIcon(beauty_icon)
+    } else if (category === "Officials"){
+      return setIcon(official_icon)
+    } else if (category === "Location"){
+      return setIcon(location_icon)
+    } else if (category === "Dress & Accessories"){
+      return setIcon(dress_icon)
+    } else if (category === "Invitations"){
+      return setIcon(invitation_icon)
+    } else if (category === "Gifts"){
+      return setIcon(gift_icon)
+    } 
+  }
+
   useEffect(() => {
-    storedToken
+    storedToken;
     axios
       .get(`${import.meta.env.VITE_API_URL}/api/vendors/${vendorId}`, {
         headers: { Authorization: `Bearer ${storedToken}` },
       })
       .then((response) => {
         setVendor(response.data);
+        setImage(response.data.typeOfService)
       })
       .catch((error) => {
         console.error("Error fetching vendor details:", error);
@@ -24,55 +65,79 @@ function VendorDetails() {
   }, [vendorId]);
 
   const deleteVendor = () => {
-    storedToken
+    storedToken;
 
     const confirmDelete = window.confirm(
       "Are you sure you want to delete this vendor?"
     );
 
-    if(confirmDelete) {
+    if (confirmDelete) {
       axios
-      .delete(`${import.meta.env.VITE_API_URL}/api/vendors/${vendor._id}`, {
-        headers: { Authorization: `Bearer ${storedToken}` },
-      })
-      .then(() => {
-        toast.success("Deleted successfully");  
-        console.log("Vendor deleted");
-        navigate(`/VendorList`);
-      })
-      .catch((error) => {
-        console.error("Error deleting vendor:", error);
-      });
+        .delete(`${import.meta.env.VITE_API_URL}/api/vendors/${vendor._id}`, {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        })
+        .then(() => {
+          toast.success("Deleted successfully");
+          console.log("Vendor deleted");
+          navigate(`/VendorList`);
+        })
+        .catch((error) => {
+          console.error("Error deleting vendor:", error);
+        });
     }
   };
 
   return vendor ? (
     <>
-      <div>
-        <h2>Vendor Details</h2>
-        <p>Name: {vendor.name}</p>
-        <p>Location: {vendor.location}</p>
-        <p>Web: {vendor.URL}</p>
-        <p>Description: {vendor.description}</p>
-        <p>Type of service: {vendor.typeOfService}</p>
-        <p>Email: {vendor.email}</p>
-        <p>Phone: {vendor.phoneNumber}</p>
-      </div>
+      <div className="details-container">
+        <div className="avatar">
+          <div className="w-24 rounded-full">
+            <img src={icon} />
+          </div>
+        </div>
 
-      <div className="mt-4">
-        <Link to={`/VendorEdit/${vendor._id}`}>
-          <button className="btn btn-xs sm:btn-sm md:btn-md btn-wide">
-            Edit
-          </button>
-        </Link>
-      </div>
+        <div className="details-name">
+          <h1>
+            {vendor.name}
+          </h1>
+        </div>
 
-      <button
-        className="btn btn-xs sm:btn-sm md:btn-md btn-wide "
-        onClick={deleteVendor}
-      >
-        Delete
-      </button>
+        <div className="details-text">
+          <p>Location: {vendor.location}</p>
+          <p>Description: {vendor.description}</p>
+          <p>Type of service: {vendor.typeOfService}</p>
+        </div>
+
+        <div className="details-text">
+          <h3>Contact information:</h3>
+        <p>{vendor.email}</p>
+          <p>{vendor.phoneNumber}</p>
+          <p>{vendor.URL}</p>
+        </div>
+
+        <div className="details-btn">
+          <Link to={`/VendorEdit/${vendor._id}`}>
+            <button className="btn">
+            <img
+          className="details-icons"
+          src={edit_icon}
+          al="EditGuest"
+          />
+            </button>
+          </Link>
+        </div>
+
+        <button
+          className="btn"
+          onClick={deleteVendor}
+        >
+         <img
+          className="details-icons"
+          src={delete_icon}
+          al="DeleteGuest"
+          />
+        </button>
+      </div>
     </>
   ) : (
     <p>loading</p>
