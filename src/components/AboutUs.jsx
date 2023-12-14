c578ac5bfa299e715a9265b1cf65c68778991a48
Júlia Sendra <juliasendra98@gmail.com>
2023-12-15 00:23:09 +0100

    improve styling in About Us & Contact pages

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,44 +1,74 @@
-import React from 'react';
-import júlia_profile_photo from "../assets/júlia-profile-photo.png"
+import React from "react";
+import julia_profile_photo from "../assets/julia-profile-photo.png";
+import elise_profile_photo from "../assets/elise-profile-photo.png";
 
 const AboutUs = () => {
   return (
     <div>
-    <div className="about-us-container">
-      <h1>About Us</h1>
-      <p>We are Elise Jonkers and Júlia Sendra, two junior full-stack developers. Together, we've created this platform in just a week to simplify wedding planning for couples.</p>
-      
-      <p>Our goal is straightforward: to provide a comprehensive set of tools that streamline the wedding planning process. We understand the importance of this day and aim to make it stress-free and enjoyable for you.</p>
+      <div className="about-us-container">
+        <h1>About Us</h1>
+        <p className="landing-pg-txt-mgn">
+          We are Elise Jonkers and Júlia Sendra, two junior full-stack
+          developers. Together, we've created this platform in just a week to
+          simplify wedding planning for couples. Our goal is straightforward: to
+          provide a comprehensive set of tools that streamline the wedding
+          planning process. We understand the importance of this day and aim to
+          make it stress-free and enjoyable for you. Thank you for choosing our
+          platform to assist you in this special journey. We're excited to be a
+          part of your wedding planning experience!
+        </p>
+      </div>
 
-      <p>Thank you for choosing our platform to assist you in this special journey. We're excited to be a part of your wedding planning experience!</p>
+      <div className="flex justify-center">
+        <div
+          className="card w-full bg-base-100 shadow-xl grid grid-cols-2"
+          style={{ margin: "20px", padding: "20px", paddingBottom: "60px" }}
+        >
+          <figure className="px-10 pt-10 col-span-1">
+            <img
+              src={elise_profile_photo}
+              alt="Shoes"
+              className="rounded-xl"
+              style={{ width: "200px", height: "266px" }}
+            />
+          </figure>
+          <div className="card-body col-span-1 flex flex-col justify-center">
+            <h2 className="card-title">Elise Jonkers</h2>
+            <p className="text-left">
+              Even though I am from a small town in the north of the
+              Netherlands, I love to discover the world. I aim to do this
+              through the beauty of traveling, especially to Latin America. New
+              cultures, places and people give me the opportunity to step
+              outside of my comfort zone and enrich my life.
+            </p>
+          </div>
+        </div>
+
+        <div
+          className="card w-full bg-base-100 shadow-xl grid grid-cols-2"
+          style={{ margin: "20px", padding: "20px", paddingBottom: "60px" }}
+        >
+          <div className="card-body col-span-1 flex flex-col justify-center">
+            <h2 className="card-title">Júlia Sendra</h2>
+            <p className="text-left">
+              I am from a town in Valencia and have a profound love for
+              photography and activities in nature. Exploring the outdoors,
+              capturing moments through the lens, and immersing myself in the
+              beauty of natural landscapes are passions that drive and inspire
+              me.
+            </p>
+          </div>
+          <figure className="px-10 pt-10 col-span-1">
+            <img
+              src={julia_profile_photo}
+              alt="Shoes"
+              className="rounded-xl"
+              style={{ width: "200px", height: "266px" }}
+            />
+          </figure>
+        </div>
+      </div>
     </div>
-
-  <div className="flex justify-center">
-  <div className="card w-full bg-base-100 shadow-xl grid grid-cols-2" style={{ margin: '20px', padding: '20px', paddingBottom: '60px' }}>
-    <figure className="px-10 pt-10 col-span-1">
-      <img src={júlia_profile_photo} alt="Shoes" className="rounded-xl" style={{ width: '200px', height: '200px' }} />
-    </figure>
-    <div className="card-body col-span-1 flex flex-col justify-center">
-      <h2 className="card-title">Elise Jonkers</h2>
-      <p className="text-left">Insert Elise's description</p>
-    </div>
-  </div>
-
-  <div className="card w-full bg-base-100 shadow-xl grid grid-cols-2" style={{ margin: '20px', padding: '20px', paddingBottom: '60px' }}>
-    <div className="card-body col-span-1 flex flex-col justify-center">
-      <h2 className="card-title">Júlia Sendra</h2>
-      <p className="text-left">I am from a town in Valencia and have a profound love for photography and activities in nature. Exploring the outdoors, capturing moments through the lens, and immersing myself in the beauty of natural landscapes are passions that drive and inspire me.</p>
-    </div>
-    <figure className="px-10 pt-10 col-span-1">
-      <img src={júlia_profile_photo} alt="Shoes" className="rounded-xl" style={{ width: '200px', height: '200px' }} />
-    </figure>
-  </div>
-</div>
-
-  </div>
-
-
-    
   );
 };
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,44 +1,92 @@
-import React from 'react';
+import React from "react";
 import linkedin_icon from "../assets/linkedin-icon.png";
 import github_icon from "../assets/github-icon.png";
 
 const Contact = () => {
   return (
     <div className="contact-page">
-    <h1>Contact Us</h1>
+      <h1>Contact Us</h1>
 
-    <div className="contact-intro">
-      <p>
-        Thank you for your interest in contacting us. Please feel free to reach out to us!
-        <br></br>
-        Below are our contact details and links to our profiles on LinkedIn and GitHub.
-      </p>
-    </div>
-
-      <div className="contact-column">
-        <h2>Elise</h2>
-      
-        <a href="https://www.linkedin.com/in/elise-jonkers-2236a1151/">
-        <img className="social-media-icons" src={linkedin_icon} alt="LinkedIn icon" />
-         </a>
-      
-        <a href="https://github.com/elisejonkers">
-        <img className="social-media-icons" src={github_icon} alt="Github icon" />
-        </a>
+      <div className="contact-intro">
+        <p className="landing-pg-txt-mgn">
+          Thank you for your interest in contacting us. Please feel free to
+          reach out to us!
+        </p>
       </div>
 
       <div className="contact-column">
-        <h2>Júlia</h2>
-      
-        <a href="https://www.linkedin.com/in/júlia-sendra-giménez/">
-        <img className="social-media-icons" src={linkedin_icon} alt="LinkedIn icon" />
-         </a>
-      
-        <a href="https://github.com/juliasendra">
-        <img className="social-media-icons" src={github_icon} alt="Github icon" />
-        </a>
+        <h2 style={{marginTop:"20px"}}>Elise</h2>
+
+        <div style={{ justifyContent: "center", display: "flex" }}>
+          <button
+            className="flex items-center space-x-2 px-4 py-2 rounded-md btn margin10"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/elise-jonkers-2236a1151/",
+                "_blank"
+              )
+            }
+          >
+            <img
+              className="social-media-icons"
+              src={linkedin_icon}
+              alt="LinkedIn icon"
+            />
+            <span>LinkedIn</span>
+          </button>
+
+          <button
+            className="flex items-center space-x-2 px-4 py-2 rounded-md btn margin10"
+            onClick={() =>
+              window.open("https://github.com/elisejonkers", "_blank")
+            }
+          >
+            <img
+              className="social-media-icons"
+              src={github_icon}
+              alt="Github icon"
+            />
+            <span>Github</span>
+          </button>
+        </div>
       </div>
-      
+
+      <div className="contact-column">
+        <h2 style={{marginTop:"20px"}}>Júlia</h2>
+
+        <div style={{ justifyContent: "center", display: "flex" }}>
+          <button
+            className="flex items-center space-x-2 px-4 py-2 rounded-md btn margin10"
+            onClick={() =>
+              window.open(
+                "https://www.linkedin.com/in/júlia-sendra-giménez/",
+                "_blank"
+              )
+            }
+          >
+            <img
+              className="social-media-icons"
+              src={linkedin_icon}
+              alt="LinkedIn icon"
+            />
+            <span>LinkedIn</span>
+          </button>
+
+          <button
+            className="flex items-center space-x-2 px-4 py-2 rounded-md btn margin10"
+            onClick={() =>
+              window.open("https://github.com/juliasendra", "_blank")
+            }
+          >
+            <img
+              className="social-media-icons"
+              src={github_icon}
+              alt="Github icon"
+            />
+            <span>Github</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
